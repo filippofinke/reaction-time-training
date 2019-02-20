e47dc7d14ab56cecaef4d1d85caf4a5d4409dcb6
filippofinke <filippo.finke@samtrevano.ch>
2019-02-20 13:21:29 +0100

    Added config.ino file

---
+++ Code/Global/config.ino
@@ -0,0 +1,12 @@
+/*
+   config
+   Configuration file for the main program.
+
+   @author Filippo Finke
+*/
+
+#define SIZE 12
+#define JUNIOR_SIZE 8
+int buttonPins[SIZE] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44};
+int ledPins[SIZE] =    {23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45};
+int juniorPins[JUNIOR_SIZE] = {22, 24, 26, 28, 30, 32, 42, 44};
