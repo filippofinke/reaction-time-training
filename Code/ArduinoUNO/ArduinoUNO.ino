f7eb5f3b0b0b9d2eb7fb0fae5e2b307c2e2bcb41
filippofinke <filippo.finke@samtrevano.ch>
2019-02-22 15:33:43 +0100

    Added support to the second 4x7segment

--- Code/ArduinoUNO/ArduinoUNO.ino
+++ Code/ArduinoUNO/ArduinoUNO.ino
@@ -1,6 +1,10 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
+#include "SevSeg.h"
+
+SevSeg sevenSeg;
+
 Adafruit_7segment matrix = Adafruit_7segment();
 
 void setup() {
@@ -9,16 +13,49 @@ void setup() {
   Serial.begin(9600);
   Serial.println("ArduinoUNO Avviato!");
   matrix.begin(0x70);
-}
-int num = 0;
-void loop() {
+  int displayType = COMMON_CATHODE;
 
-  Serial.println(num);
-  matrix.writeDigitNum(0, (num / 1000), false);
-  matrix.writeDigitNum(1, (num / 100) % 10, false);
-  matrix.writeDigitNum(3, (num / 10) % 10, true);
-  matrix.writeDigitNum(4, num % 10, false);
-  matrix.writeDisplay();
+  int digit1 = 10; //Pin 12 on my 4 digit display
+  int digit2 = 11; //Pin 9 on my 4 digit display
+  int digit3 = 12; //Pin 8 on my 4 digit display
+  int digit4 = 13; //Pin 6 on my 4 digit display
+
+  int segA = 6; //Pin 11 on my 4 digit display
+  int segB = 8; //Pin 7 on my 4 digit display
+  int segC = 2; //Pin 4 on my 4 digit display
+  int segD = 5; //Pin 2 on my 4 digit display
+  int segE = 4; //Pin 1 on my 4 digit display
+  int segF = 7; //Pin 10 on my 4 digit display
+  int segG = 3; //Pin 5 on my 4 digit display
+  int segDP= 5; //Pin 3 on my 4 digit display
+
+  int numberOfDigits = 4;
+
+  sevenSeg.Begin(displayType, numberOfDigits, digit1, digit2, digit3, digit4, segA, segB, segC, segD, segE, segF, segG, segDP);
+  
+  sevenSeg.SetBrightness(100);
+  
+}
+long tempo = 0;
+long points = 0;
+boolean updateNeeded = true;
+
+void loop() {
+  if(updateNeeded) {
+    matrix.writeDigitNum(0, (tempo / 1000), false);
+    matrix.writeDigitNum(1, (tempo / 100) % 10, false);
+    matrix.writeDigitNum(3, (tempo / 10) % 10, true);
+    matrix.writeDigitNum(4, tempo % 10, false);
+    matrix.writeDisplay();
+    updateNeeded = false;
+    Serial.print("TEMPO :");
+    Serial.println(tempo);
+    Serial.print("PUNTI: ");
+    Serial.println(points);
+  }
+  char tempString[10]; 
+  sprintf(tempString, "%04d", points);
+  sevenSeg.DisplayString(tempString, 3);
   delay(10);
 }
 
@@ -37,6 +74,11 @@ void receiveEvent(int bytes) {
   value = (value << 8) | d;
   if (s == 0)
   {
-    num = value;
+    tempo = value;
   }
+  else if(s==1)
+  {
+    points = value;
+  }
+  updateNeeded = true;
 }
