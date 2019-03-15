e50c1cfa34f042e22df5f2254f5bd4725d1181d4
filippofinke <filippo.finke@samtrevano.ch>
2019-03-15 15:24:24 +0100

    Added c_songs.ino

---
+++ Code/Global/c_songs.ino
@@ -0,0 +1,40 @@
+const int c = 261;
+const int d = 294;
+const int e = 329;
+const int f = 349;
+const int g = 391;
+const int gS = 415;
+const int a = 440;
+const int aS = 455;
+const int b = 466;
+const int cH = 523;
+const int cSH = 554;
+const int dH = 587;
+const int dSH = 622;
+const int eH = 659;
+const int fH = 698;
+const int fSH = 740;
+const int gH = 784;
+const int gSH = 830;
+const int aH = 880;
+
+void beep(int note, int duration)
+{
+  tone(buzzerPin, note);
+  delay(duration);
+  noTone(buzzerPin);
+
+}
+ 
+void winSong()
+{
+  beep(a, 500);
+  beep(a, 500);    
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);  
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+}
